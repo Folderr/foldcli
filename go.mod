3c2a72903d8d6612e1ed433bcc74d93c72af9add
Aervyon <null@voidnull.dev>
2023-04-20 03:32:44 -0500

    Updated go module files to correct repo

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/Folderr/Manager
+module github.com/Folderr/folderr-cli
 
 go 1.20
 
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2023 Folderr <contact@folderr.net>
 package main
 
 import (
-	"github.com/Folderr/Manager/cmd"
+	"github.com/Folderr/folderr-cli/cmd"
 )
 
 func main() {
