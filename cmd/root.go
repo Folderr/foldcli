5c9f2651dc3b66ad0f8d8b724ce83fa0497199a7
Aervyon <aervy@aervyon.com>
2023-11-12 00:10:39 -0600

    bump version due to change in the way keys work

--- cmd/root.go
+++ cmd/root.go
@@ -25,7 +25,7 @@ var rootCmd = &cobra.Command{
 	Long: `A CLI to setup and manage your Folderr instance. Get started with:
 ` + rootCmdName + ` init /home/folderr/folderr https://github.com/Folderr/<repo>
 ` + rootCmdName + ` init`,
-	Version: "Alpha 0.0.3",
+	Version: "Alpha 0.0.4",
 	CompletionOptions: cobra.CompletionOptions{
 		DisableDefaultCmd: true,
 	},
